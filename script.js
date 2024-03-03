d7ad50c9adaea925db91365a903634c7bad0e889
Ganesh Pund <ganupund7787@gmail.com>
2024-03-03 10:59:05 -0800

    Image change

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ setInterval(function () {
     hrotation = 30 * htime + mtime / 2;
     mrotation = 6 * mtime;
     srotation = 6 * stime;
-    
+
     hour.style.transform = `rotate(${hrotation}deg)`
     minutes.style.transform = `rotate(${mrotation}deg)`
     seconds.style.transform = `rotate(${srotation}deg)`
